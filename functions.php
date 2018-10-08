46fc13406e0bf0a7fd334249fff1e2adde8074b8
cjwd <chinarakinda@gmail.com>
2018-10-08 10:08:05 -0400

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function divi_child_enqueue_style() {
+  $parent_style = "divi-style";
+
+  wp_enqueue_style( $parent_style. get_template_directory_uri() . '/style.css');
+  wp_enqueue_style( 'divichild-style', get_stylesheet_directory_uri() . '/style.css', [$parent_style], wp_get_theme()->get('Version'));
+}
+add_action('wp_enqueue_scripts', 'divi_child_enqueue_styles');
